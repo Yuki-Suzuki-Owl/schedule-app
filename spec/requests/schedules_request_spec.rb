e3550d585478e800edaa78aef61632172bd49e45
Yuki Suzuki <tobiuo1994@gmail.com>
2020-11-06 13:17:01 +0900

    system spec作成前

--- spec/requests/schedules_request_spec.rb
+++ spec/requests/schedules_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
-
-RSpec.describe "Schedules", type: :request do
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/schedules/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# system specを書くためコ一旦メントアウト
+# RSpec.describe "Schedules", type: :request do
+#
+#   describe "GET /show" do
+#     it "returns http success" do
+#       get "/schedules/show"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
-
-RSpec.describe "Users", type: :request do
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# system specを書くためコ一旦メントアウト
+# RSpec.describe "Users", type: :request do
+#
+#   describe "GET /new" do
+#     it "returns http success" do
+#       get "/users/new"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+#
+#   describe "GET /edit" do
+#     it "returns http success" do
+#       get "/users/edit"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
 end
